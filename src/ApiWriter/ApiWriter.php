ea06614f2b90c347e88984cedbdd0b293abcf802
Kristof Torfs <kristof@torfs.org>
2024-04-25 15:42:07 +0200

    Add profiler token when available #GH-9
    Resolves GH-9

--- src/ApiWriter/ApiWriter.php
+++ src/ApiWriter/ApiWriter.php
@@ -4,27 +4,45 @@ declare(strict_types=1);
 
 namespace TorfsICT\Bundle\CodeMonitoringBundle\ApiWriter;
 
+use Symfony\Component\Console\Debug\CliRequest;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
+use Symfony\Component\HttpKernel\Profiler\Profile;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
+use Symfony\Component\Stopwatch\Stopwatch;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use TorfsICT\Bundle\CodeMonitoringBundle\Exception\CaughtException;
 use TorfsICT\Bundle\CodeMonitoringBundle\ExceptionRenderer\ExceptionRenderer;
 
-final readonly class ApiWriter
+final class ApiWriter
 {
-    private string $url;
-    private bool $useSpool;
+    private readonly string $url;
+
+    private readonly bool $useSpool;
+
+    /**
+     * @var \SplObjectStorage<\Throwable, \Throwable>
+     */
+    private \SplObjectStorage $queue;
 
     public function __construct(
-        private HttpClientInterface $httpClient,
-        private ExceptionRenderer $renderer,
-        private string $endpoint,
-        private string $project,
-        private string $environment,
-        private string $secret,
-        private ?string $spool,
+        private readonly HttpClientInterface $httpClient,
+        private readonly ExceptionRenderer $renderer,
+        private readonly RequestStack $requestStack,
+        private readonly ?Stopwatch $stopwatch,
+        private readonly ?Profiler $profiler,
+        private readonly string $endpoint,
+        private readonly string $project,
+        private readonly string $environment,
+        private readonly string $secret,
+        private readonly ?string $spool,
     ) {
         $this->url = sprintf('%s/monitoring', $this->endpoint);
-
         $this->useSpool = null !== $this->spool;
+        $this->queue = new \SplObjectStorage();
 
         if ($this->useSpool && !is_dir((string) $this->spool)) {
             throw new \RuntimeException(sprintf('Spool directory "%s" does not exist.', $this->spool));
@@ -33,9 +51,7 @@ final readonly class ApiWriter
 
     public function exception(\Throwable $throwable): void
     {
-        $json = $this->toArray($throwable, true);
-        $json['caught'] = $throwable instanceof CaughtException;
-        $this->process('exception', $json);
+        $this->queue->attach($throwable);
     }
 
     public function deprecation(\Throwable $throwable): void
@@ -141,6 +157,53 @@ final readonly class ApiWriter
 
             $this->post($this->url.'/'.$type, $json);
             unlink($renamed);
+        }
+    }
+
+    public function onKernelTerminate(TerminateEvent $event): void
+    {
+        $this->onTerminate($event->getRequest(), $event->getResponse());
+    }
+
+    public function onConsoleTerminate(ConsoleTerminateEvent $event): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        if (!$request instanceof CliRequest || $request->command !== $event->getCommand()) {
+            return;
+        }
+
+        if (null !== $sectionId = $request->attributes->get('_stopwatch_token')) {
+            // we must close the section before saving the profile to allow late collect
+            try {
+                assert(is_string($sectionId));
+                $this->stopwatch?->stopSection($sectionId);
+            } catch (\LogicException) {
+                // noop
+            }
+        }
+
+        $request->command->exitCode = $event->getExitCode();
+        $request->command->interruptedBySignal = $event->getInterruptingSignal();
+
+        $this->onTerminate($request, $request->getResponse());
+    }
+
+    private function onTerminate(Request $request, Response $response): void
+    {
+        foreach ($this->queue as $throwable) {
+            $json = $this->toArray($throwable, true);
+            $json['caught'] = $throwable instanceof CaughtException;
+
+            if (null !== $this->profiler) {
+                $profile = $this->profiler->collect($request, $response, $throwable);
+                if ($profile instanceof Profile) {
+                    $this->profiler->saveProfile($profile);
+                    $json['token'] = $profile->getToken();
+                }
+            }
+
+            $this->process('exception', $json);
+            $this->queue->detach($throwable);
         }
     }
 }
